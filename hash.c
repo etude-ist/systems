db25920934a976100b747a1bdb59d7ed008109c6
etude-ist <logika01@gmail.com>
2020-05-15 22:30:37 +0200

    add hash by multi

--- hash.c
+++ hash.c
@@ -1,17 +1,32 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
 const int BASE = 256;
+const int MULTIPLIER = 37;
 
 size_t hash_by_divide(const char *s, size_t m) {
   size_t h;
   unsigned const char *us;
 
-  us = (unsigned const char*) s;
+  us = (unsigned const char *)s;
 
   h = 0;
   while (*us != '\0') {
     h = (h * BASE + *us) % m;
+    us++;
+  }
+
+  return h;
+}
+
+size_t hash_by_multi(const char *s) {
+  size_t h;
+  unsigned const char *us;
+  us = (unsigned const char *)s;
+
+  h = 0;
+  while (*us != '\0') {
+    h = h * MULTIPLIER + *us;
     us++;
   }
 
@@ -26,6 +41,15 @@ void test_hash_by_divide() {
   printf("test_hash_by_divide OK\n");
 }
 
+void test_hash_by_multiplication() {
+  assert(hash_by_multi("Wang Miao") == hash_by_multi("Wang Miao"));
+  assert(hash_by_multi("Ye Wenjie") == hash_by_multi("Ye Wenjie"));
+  assert(hash_by_multi("Ye Wenjie") != hash_by_multi("ye wenjie"));
+  assert(hash_by_multi("2008") == hash_by_multi("2008"));
+  printf("test_hash_by_multiplication OK\n");
+}
+
 int main() {
   test_hash_by_divide();
+  test_hash_by_multiplication();
 }
